c42e502ecfecbf9b3efe35330d6c4382359cc8e0
seanmun <smunley13@gmail.com>
2025-02-20 08:51:19 -0600

    config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
   basePath: '',
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
 }
 
-module.exports = nextConfig
+export default nextConfig
